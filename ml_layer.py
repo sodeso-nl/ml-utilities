8f0a9fa6489d9d1d760bf05178022504142a78dd
Ronald Mathies <github@sodeso.nl>
2023-05-23 21:53:03 +0200

    Added save_model_alt and load_model_alt to circumvent certain issues in TensorFlow. Fixed some methods in the ml_layer and added a ml_layer notebook to test some of the functions.

--- ml_layer.py
+++ ml_layer.py
@@ -1,3 +1,9 @@
+import os
+import pickle
+
+from keras import Model
+
+
 def collect_layers(model, recursive=True, include_trainable=True, include_non_trainable=True):
     """
     Collects layers from the given model, if the model is functional then there might be sub-branches
@@ -51,18 +57,58 @@ def set_trainable_on_layers(model, layer_names=None, trainable=True):
 
 
 def set_trainable_on_first_n_layers(model, n, trainable=True):
-    for layer in model.layers[:-n]:
+    layers = collect_layers(model, recursive=True, include_trainable=True, include_non_trainable=True)
+    for layer in layers[:n]:
         layer.trainable = trainable
 
 
 def set_trainable_on_last_n_layers(model, n, trainable=True):
-    for layer in model.layers[-n:]:
+    layers = collect_layers(model, recursive=True, include_trainable=True, include_non_trainable=True)
+    for layer in layers[-n:]:
         layer.trainable = trainable
+
+
+def list_model(model, recursive=True, include_trainable=True, include_non_trainable=True):
+    layers = collect_layers(model, recursive=recursive, include_trainable=include_trainable, include_non_trainable=include_non_trainable)
+    list_layers(layers=layers, include_trainable=include_trainable, include_non_trainable=include_non_trainable)
+
+
+def list_layers(layers, include_trainable=True, include_non_trainable=True):
+    layer_name_col_width = len(max(list(map(lambda l: l.name, layers)), key=len))
+    layer_type_col_width = len(max(list(map(lambda l: type(l).__name__, layers)), key=len))
+    layer_shape_col_width = len(max(list(map(lambda l: str(l.output_shape), layers)), key=len))
+
+    print(f"{'Row':<5} | {'Name (Type)':<{layer_name_col_width + layer_type_col_width + 3}} | Trainable | Output Shape")
+    for layer_number, layer in enumerate(layers):
+        if (include_trainable and layer.trainable) or (include_non_trainable and layer.trainable is False):
+            print(f"{layer_number:<5} | {layer.name:<{layer_name_col_width}} ({type(layer).__name__:<{layer_type_col_width}}) | {str(layer.trainable):<9} | "
+                  f"{str(layer.output_shape):<{layer_shape_col_width}}")
+
+
+########################################################################################################################
+# Saving / loading models
+########################################################################################################################
+
+
+def save_weights(model, filepath, save_format="h5"):
+    """
+    Saves
+    :param model:
+    :param filepath:
+    :param save_format:
+    :return:
+    """
+    path = os.path.dirname(filepath)
+    if path:
+        os.makedirs(path, exist_ok=True)
+    model.save(filepath, save_format=save_format)
 
 
 def load_weights(model, filepath):
     """
-    Disables trainable on the model and on all the layers.
+    Disables trainable on the model and on all the layers, then loads the weights and
+    restores the trainable configuration on the model and weights.
+
     :param model: the model to load the weights into
     :param filepath: String or PathLike, path to the file to save the weights
                 to. When saving in TensorFlow format, this is the prefix used
@@ -77,19 +123,49 @@ def load_weights(model, filepath):
     set_trainable_on_layers(model, trainable=False)
 
     # Load the weights
-    model.load_weights(filepath)
+    model.load_weights(filepath,)
 
     set_trainable_on_layers(model, layer_names=trainable_layer_names, trainable=True)
 
 
-def list_layers(model, recursive=True, include_trainable=True, include_non_trainable=True):
-    layers = collect_layers(model, recursive=recursive, include_trainable=include_trainable, include_non_trainable=include_non_trainable)
-    layer_name_col_width = len(max(list(map(lambda l: l.name, layers)), key=len))
-    layer_type_col_width = len(max(list(map(lambda l: type(l).__name__, layers)), key=len))
-    layer_shape_col_width = len(max(list(map(lambda l: str(l.output_shape), layers)), key=len))
+def save_model_alt(model, directory, name, format='h5'):
+    """
+    Alternative solution to saving a model since the default implementation has issues with augemntation layers.
 
-    print(f"{'Row':<5} | {'Name (Type)':<{layer_name_col_width + layer_type_col_width + 3}} | Trainable | Output Shape")
-    for layer_number, layer in enumerate(layers):
-        if (include_trainable and layer.trainable) or (include_non_trainable and layer.trainable is False):
-            print(f"{layer_number:<5} | {layer.name:<{layer_name_col_width}} ({type(layer).__name__:<{layer_type_col_width}}) | {str(layer.trainable):<9} | "
-                  f"{str(layer.output_shape):<{layer_shape_col_width}}")
+    Use in conjunction with load_model_alt
+
+    :param model: The model to save
+    :param directory: The target directory to save the model to
+    :param name: The name of the model, used for filenames, use without extension
+    :param format: The format to store the data (tf/h5)
+    """
+    config = model.get_config()
+
+    # Check if target directory exists, if not, create it
+    os.makedirs(directory, exist_ok=True)
+
+    config_file = os.path.join(directory, name + '.pkl')
+    with open(config_file, 'wb') as fp:
+        pickle.dump(config, fp)
+
+    model_file = os.path.join(directory, name + '.' + format)
+    model.save(model_file, save_format="tf")
+
+
+def load_model_alt(directory, name, format='h5'):
+    """
+    Alternative solution to saving a model since the default implementation has issues with augemntation layers.
+
+    Use in conjunction with save_model_alt
+
+    :param directory: The target directory to save the model to
+    :param name: The name of the model, used for filenames, use without extension
+    :param format: The format to store the data (tf/h5)
+    :return: the loaded model
+    """
+    config_file = os.path.join(directory, name + '.pkl')
+    with open(config_file, 'rb') as fp:
+        model = Model.from_config(pickle.load(fp))
+        model_file = os.path.join(directory, name + '.' + format)
+        model.load_weights(model_file)
+        return model

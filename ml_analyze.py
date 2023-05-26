f3b5c7c61238c379c4b733fca0e2bc6abe043134
Ronald Mathies <github@sodeso.nl>
2023-05-26 13:45:22 +0200

    Added ml_analyze.py

--- ml_analyze.py
+++ ml_analyze.py
@@ -3,7 +3,7 @@ import ml_internal as mlint
 from sklearn.metrics import accuracy_score
 
 
-def calculate_accuract(y_true, y_pred):
+def calculate_accuracy(y_true, y_pred):
     # If y_true or y_pred is not a Numpy array then try to convert it.
     y_true = mlint.convert_to_numpy_array_if_neccesary(y_true)
     y_pred = mlint.convert_to_numpy_array_if_neccesary(y_pred)

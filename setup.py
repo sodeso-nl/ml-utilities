76e1bed5bb7d1f9bec55e0ffe3c1ade05b564133
Ronald Mathies <github@sodeso.nl>
2023-08-30 15:19:44 +0200

    Converted to library for pypi.

--- setup.py
+++ setup.py
@@ -14,10 +14,10 @@ setup(
     long_description="A various set of utility functions that can be used in conjunction with Tensorflow.",
     packages=find_packages(),
     install_requires=[
-        # 'scikit-learn>=1.2.2',
-        # 'matplotlib>=3.7.1',
-        # 'pandas>=2.0.0',
-        # 'tensorflow>=2.8.0'
+        'scikit-learn>=1.2.2',
+        'matplotlib>=3.7.1',
+        'pandas>=2.0.0',
+        'tensorflow>=2.8.0'
     ],
     keywords=['python', 'machine', 'deep', 'learning', 'tensorflow'],
     classifiers=[

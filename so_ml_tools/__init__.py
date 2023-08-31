7ae8c8d4d0ee0e62d130e558d37f7637029bb2c0
Ronald Mathies <github@sodeso.nl>
2023-08-31 15:47:25 +0200

    Converted to library for pypi.

--- so_ml_tools/__init__.py
+++ so_ml_tools/__init__.py
@@ -4,6 +4,8 @@ import so_ml_tools.multiclass
 import so_ml_tools.regression
 import so_ml_tools.util
 import so_ml_tools.nlp
+import so_ml_tools.nlp.embedding
 import so_ml_tools.pandas
 import so_ml_tools.tf
+import so_ml_tools.tf.model
 import so_ml_tools.data
--- so_ml_tools/tf/__init__.py
+++ so_ml_tools/tf/__init__.py
@@ -1,3 +1,2 @@
 import so_ml_tools.tf.dataset
 import so_ml_tools.tf.tensor
-import so_ml_tools.tf.model

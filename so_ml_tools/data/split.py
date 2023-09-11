e9aa9a9df35ffab04adfcfaa265022d264e59f2f
Ronald Mathies <github@sodeso.nl>
2023-09-11 14:11:53 +0200

    Fixed a minor issue.

--- so_ml_tools/data/split.py
+++ so_ml_tools/data/split.py
@@ -1,4 +1,4 @@
-import sklearn as _sklearn
+from sklearn import model_selection as _model_selection
 
 
 def split_train_test_data(*arrays, test_size=.2, train_size=.8, random_state=42, shuffle=True):
@@ -11,7 +11,7 @@ def split_train_test_data(*arrays, test_size=.2, train_size=.8, random_state=42,
     assert test_size + train_size == 1., f"The sum of test_size: {test_size} and train_size: {train_size} does not " \
                                          f"add up to 1, not all data is used."
 
-    return _sklearn.model_selection.train_test_split(*arrays,
+    return _model_selection.train_test_split(*arrays,
                                                     test_size=test_size,
                                                     train_size=train_size,
                                                     random_state=random_state,

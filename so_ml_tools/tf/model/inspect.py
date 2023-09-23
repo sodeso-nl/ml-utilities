9ca6b8d29f33218143425ed9c602bd90fe6b74c1
Ronald Mathies <github@sodeso.nl>
2023-09-23 10:20:05 +0200

    Updated the batch_and_prefetch method to check if not already batched.

--- so_ml_tools/tf/model/inspect.py
+++ so_ml_tools/tf/model/inspect.py
@@ -88,21 +88,30 @@ def list_layers(layers: list[_tf.keras.layers.Layer], include_trainable=True, in
     layer_shape_col_width = len(max(list(map(lambda l: str(l.output_shape), layers)), key=len))
     layer_dtype_col_width = len(max(list(map(lambda l: str(l.dtype), layers)), key=len))
     layer_dtype_policy_col_width = len(max(list(map(lambda l: str(l.dtype_policy.name), layers)), key=len))
+    layer_variables_col_width = len(max(list(map(lambda l: str(l.variables), layers)), key=len))
 
     print(
         f"{'row':<5} | "
         f"{'name (type)':<{layer_name_col_width + layer_type_col_width + 3}} | "
         f"{'dtype':<{layer_dtype_col_width}} | "
         f"{'policy':<{layer_dtype_policy_col_width}} | "
-        f"trainable | {'output shape':<{layer_shape_col_width}} | "
-        f"Param #")
+        f"trainable | "
+        f"{'output shape':<{layer_shape_col_width}} | "
+        f"{'Total Param #':<{layer_variables_col_width}} |"
+        f"{'Trainable Param #':<{layer_variables_col_width}} |"
+        f"{'Non-trainable Param #':<{layer_variables_col_width}}")
     for layer_number, layer in enumerate(layers):
         if (include_trainable and layer.trainable) or (include_non_trainable and layer.trainable is False):
-            params = sum([_tf.size(var).numpy() for var in layer.trainable_variables])
+            total_params = sum([_tf.size(var).numpy() for var in layer.variables])
+            trainaible_params = sum([_tf.size(var).numpy() for var in layer.trainable_variables])
+            non_trainable_params = total_params - trainaible_params
             print(
                 f"{layer_number:<5} | "
                 f"{layer.name:<{layer_name_col_width}} ({type(layer).__name__:<{layer_type_col_width}}) | "
                 f"{str(layer.dtype):<{layer_dtype_col_width}} | "
                 f"{str(layer.dtype_policy.name):<{layer_dtype_policy_col_width}} | "
                 f"{str(layer.trainable):<9} | "
-                f"{str(layer.output_shape):<{layer_shape_col_width}} | {params}")
+                f"{str(layer.output_shape):<{layer_shape_col_width}} |"
+                f"{str(total_params):<{layer_variables_col_width}} |"
+                f"{str(trainaible_params):<{layer_variables_col_width}} |"
+                f"{str(non_trainable_params):<{layer_variables_col_width}}")

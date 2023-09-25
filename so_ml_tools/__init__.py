141ec298508dbf749829ed8d5e91d66eb2833e34
Ronald Mathies <github@sodeso.nl>
2023-09-25 20:19:55 +0200

    Updated reduce_lr_on_plateau_callback

--- so_ml_tools/__init__.py
+++ so_ml_tools/__init__.py
@@ -10,7 +10,7 @@ from . import (
 # We follow Semantic Versioning (https://semver.org/spec/v2.0.0.html)
 _MAJOR_VERSION = '0'
 _MINOR_VERSION = '1'
-_PATCH_VERSION = '55'
+_PATCH_VERSION = '56'
 
 __version__ = '.'.join([
     _MAJOR_VERSION,
--- so_ml_tools/tf/model/augment.py
+++ so_ml_tools/tf/model/augment.py
@@ -1,13 +1,15 @@
 import keras as _keras
+import so_ml_tools as _soml
 
 
 def create_augmentation_layer(random_flip_h=True,
                               random_flip_v=True,
                               random_rotate=True,
-                              random_height=False,
-                              random_width=False,
+                              random_height=True,
+                              random_width=True,
                               random_translation=True,
-                              random_zoom=True) -> _keras.Sequential:
+                              random_zoom=True,
+                              verbose=1) -> _keras.Sequential:
     steps = []
     if random_flip_h or random_flip_v:
         if random_flip_h and random_flip_v:
@@ -32,4 +34,8 @@ def create_augmentation_layer(random_flip_h=True,
     if random_translation:
         steps.append(_keras.layers.RandomTranslation(height_factor=0.1, width_factor=0.1))
 
-    return _keras.Sequential(steps, name='data_augmentation')
+    augmentation_layers = _keras.Sequential(steps, name='data_augmentation')
+    if verbose:
+        _soml.tf.model.inspect.list_model(model=augmentation_layers, recursive=False)
+
+    return augmentation_layers

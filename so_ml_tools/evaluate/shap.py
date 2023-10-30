212d0442751e30f63ce4320f77ac8207498bacd8
Ronald Mathies <github@sodeso.nl>
2023-10-30 11:13:15 +0100

    Added dataframe generate_code_ordinal_encoder

--- so_ml_tools/evaluate/shap.py
+++ so_ml_tools/evaluate/shap.py
@@ -18,19 +18,28 @@ def calculate_shap_values(explainer: _sh.Explainer, x, n_samples: [int | str] = 
     return explainer.shap_values(x, nsamples=n_samples)
 
 
-def waterfall(explainer: _sh.Explainer, shap_values, plot_class: int, feature_names: list[str], max_features: [str | int] = 'auto'):
+def waterfall(explainer: _sh.Explainer, shap_values, feature_names: list[str], max_features: [str | int] = 'auto',
+              plot_class: int = None):
     """
     Plot the breakdown of the 
 
     Args:
+      explainer: the explainer object
       shap_values: calculated values, use shap_values[x] for single class.
-      class_names: a `list` containing the class names.
       feature_names: a `list` containing the feature names.
-      plot_type: 'bar' for multiclass and single class, 'dot', 'violin' and  for single class.
+      max_features: maximum number of features to display ordered by most imported to least.
+      plot_class: (optional) the class to plot
 
     Returns:
         None
     """
+    assert len(shap_values) > 1 and plot_class is None, (f"shap_values contains multiple classes ({len(shap_values)}), "
+                                                         f"the force_plot can only plot a single class at a time, "
+                                                         f"please specify the class to plot.")
+
+    if plot_class is None:
+        plot_class = 0
+
     explanation = _sh.Explanation(
         values=shap_values[plot_class],  # Actual values of specific class
         base_values=explainer.expected_value[plot_class],  # Mean value: E[f(x)] for specific class.
@@ -42,7 +51,8 @@ def waterfall(explainer: _sh.Explainer, shap_values, plot_class: int, feature_na
     _sh.waterfall_plot(shap_values=explanation, max_display=max_features)
 
 
-def summary_plot(shap_values: _np.array, class_names: list[str], feature_names: list[str], plot_type: str = 'auto') -> None:
+def summary_plot(shap_values: list[_np.array], class_names: list[str], feature_names: list[str],
+                 plot_type: str = 'auto') -> None:
     """
     Summary plot for displaying feature for all entries.
 
@@ -55,10 +65,45 @@ def summary_plot(shap_values: _np.array, class_names: list[str], feature_names: 
     Returns:
         None
     """
-    assert len(shap_values) > 1 and len(shap_values) == len(class_names), (f"Number of class_names {len(class_names)} does not match "
-                                                  f"the number of classes present in shape_values {len(shap_values)}.")
+    assert len(shap_values) > 1 and len(shap_values) == len(class_names), (f"Number of class_names {len(class_names)} "
+                                                                           f"does not match the number of classes "
+                                                                           f"present in shape_values {len(shap_values)}"
+                                                                           f", possible options:\n\nCheck if class_names"
+                                                                           f" is a list within list, if so use "
+                                                                           f"class_names[0].")
+
+    # In case the shap_values were calculated for a single set of features we need to add an additional dimension.
+    if shap_values[0].ndim == 1:
+        shap_values[0] = _np.expand_dims(shap_values[0], axis=0)
+        shap_values[1] = _np.expand_dims(shap_values[1], axis=0)
+        shap_values[2] = _np.expand_dims(shap_values[2], axis=0)
 
     _sh.summary_plot(shap_values=shap_values,
                      class_names=class_names,
                      feature_names=feature_names,
                      plot_type=plot_type)
+
+
+def force_plot(shap_values: list[_np.array], explainer: _sh.Explainer, feature_names: list[str],
+               plot_class: int = None):
+    """
+    Force plot for displaying feature for a single class.
+
+    Args:
+      shap_values: calculated values
+      explainer: the explainer object
+      feature_names: a `list` containing the feature names.
+      plot_class: (optional) the class to plot
+
+    Returns:
+        None
+    """
+    assert len(shap_values) > 1 and plot_class is None, (f"shap_values contains multiple classes ({len(shap_values)}), "
+                                                         f"the force_plot can only plot a single class at a time, "
+                                                         f"please specify the class to plot.")
+
+    if plot_class is None:
+        plot_class = 0
+
+    _sh.force_plot(base_value=explainer.expected_value[plot_class], shap_values=shap_values[plot_class],
+                   features=feature_names)

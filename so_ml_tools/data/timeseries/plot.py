5ff0bcc313f5afdefde1bc0c93a3205bc6f971df
Ronald Mathies <github@sodeso.nl>
2024-01-25 11:21:38 +0100

    Added specific plots for data/timeseries

--- so_ml_tools/data/timeseries/plot.py
+++ so_ml_tools/data/timeseries/plot.py
@@ -3,12 +3,20 @@ import seaborn as _sns
 import itertools
 
 
-def timeseries(x, y, labels: list[str] = None, title=None, x_label=None, y_label=None, label_color='black'):
-    fig, ax = _plt.subplots(figsize=(8, 6))
+def timeseries(x, y, labels: list[str] = None, title: str = None, x_label: str = None, y_label: str = None,
+               start: int = None, figsize: tuple = (8, 6), label_color='black'):
+    fig, ax = _plt.subplots(figsize=figsize)
 
     color_palette = itertools.cycle(_sns.color_palette(palette="tab10", n_colors=len(x)))
     for idx, _x in enumerate(x):
-        _sns.scatterplot(x=_x, y=y[idx], color=next(color_palette), ax=ax, label=None if labels is None else labels[idx])
+        # _sns.scatterplot(
+        _sns.lineplot(
+            x=_x[start:],
+            y=y[idx][start:],
+            color=next(color_palette),
+            # s=7,
+            ax=ax,
+            label=None if labels is None else labels[idx])
 
     if x_label:
         _plt.xlabel(x_label)

940912125a39532c527fbb0beca3d818761449df
Ronald Mathies <github@sodeso.nl>
2024-03-09 21:42:16 +0100

    Fix for confusion matrix

--- so_ml_tools/pd/report.py
+++ so_ml_tools/pd/report.py
@@ -1,14 +1,14 @@
 import pandas as _pd
 import sweetviz as _sv
-from ydata_profiling import ProfileReport as _pr
+#from ydata_profiling import ProfileReport as _pr
 
 
-def profile_report(dataframe: _pd.DataFrame) -> _pr:
-    """
-    Args:
-        dataframe:
-    """
-    return _pr(df=dataframe)
+#def profile_report(dataframe: _pd.DataFrame) -> _pr:
+    # """
+    # Args:
+    #     dataframe:
+    # """
+    # return _pr(df=dataframe)
 
 
 def sweetvis_report(dataframe: _pd, target_feat: str = None):

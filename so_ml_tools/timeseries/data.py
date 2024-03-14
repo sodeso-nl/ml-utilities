4aa5469e40e610b87e50b6871c6ce424395ea241
Ronald Mathies <github@sodeso.nl>
2024-03-14 13:35:02 +0100

    Added timeseries window function and moved some packages.

--- so_ml_tools/timeseries/data.py
+++ so_ml_tools/timeseries/data.py
@@ -2,27 +2,66 @@ import tensorflow as _tf
 import numpy as _np
 
 
-def get_labelled_windows(x, horizon):
+def make_naive_predictions(y_true):
     """
-    Creates labels for windowed dataset.
+    Method that creates a naive prediction from the given truth labels:
 
-    E.g. if horizon = 1
-    Input: [0, 1, 2, 3, 4, 5, 6, 7] -> Output: ([0, 1, 2, 3, 4, 5, 6], [7])
+    Input:
+        [0, 1, 2, 3, 4, 5, 6, 7]
+
+    Output:
+        [1, 2, 3, 4, 5, 6, 7]
+        [0, 1, 2, 3, 4, 5, 6]
+
+
+    Args:
+        y_true: the truth labels for which we want to make a naive prediction
+
+    Returns:
+        y_true and the naive prediction based on the given truth labels
+
     """
-    return x[:, :-horizon], x[:, -horizon:]
+    naive_predictions = y_true[:-1]
+    return y_true[1:], naive_predictions
 
 
 def make_windows(x, window_size, horizon):
     """
     Turns 1 1D array into a 2D array of sequential labelled windows of window_size with horizon size labels.
+
+    E.g. if horizon = 1 and window_size = 3, then
+    X = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    Output:
+        Features:
+        [
+            [0, 1, 2, 3, 4, 5, 6]
+            [1, 2, 3, 4, 5, 6, 7]
+        ]
+
+        Labels:
+        [
+            [7]
+            [8]
+        ]
+
+    Args:
+        x: A 1D Numpy array or Tensor or a single column Pandas dataframe
+        window_size: the number of time steps to use as features
+        horizon:  the number of time steps to use as labels
+
     """
     if not isinstance(x, _tf.Tensor):
         x = _tf.convert_to_tensor(value=x)
 
     # Check that we only have a single feature
-    assert x.ndim == 1, f"x contains multiple features {x.ndim}, this method only handles a single feature, use x['col'] to use a single feature."
+    assert x.ndim == 1, (f"Shape of x is not correct {x.shape}, should be 1D array (ie. [1, 2, 3, 4]), please check "
+                         f"the following:"
+                         f"* Does your data contain multiple columns, if so, select only a single column, if it is a "
+                         f"Pandas dataframe then use df['col'], if it is a tensor use slicing, for example t[:,2] to "
+                         f"select the third column, If your data is column based then convert it to row based "
+                         f"using tf.squeeze()")
 
-    # Convert the object to a numpy object so we can use slicing.
     x = x.numpy()
 
     # 1. Create a window of specific window_size (add the horizon on the end for labelling later)
@@ -32,9 +71,19 @@ def make_windows(x, window_size, horizon):
     window_indexes = window_step + _np.expand_dims(_np.arange(len(x) - (window_size + horizon - 1)),
                                                   axis=0).T  # Create 2D array of windows of window size window_size.
 
-    # 3. Index on the target arraay (a time series) with 2D array of multiple widnow steps
+    # 3. Index on the target array (a time series) with 2D array of multiple window steps
     windowed_array = x[window_indexes]
 
-    # 4. get the labelled widnows
-    windows, labels = get_labelled_windows(windowed_array, horizon=horizon)
+    # 4. get the labelled windows
+    windows, labels = _get_labelled_windows(windowed_array, horizon=horizon)
     return windows, labels
+
+
+def _get_labelled_windows(x, horizon):
+    """
+    Creates labels for windowed dataset.
+
+    E.g. if horizon = 1
+    Input: [0, 1, 2, 3, 4, 5, 6, 7] -> Output: ([0, 1, 2, 3, 4, 5, 6], [7])
+    """
+    return x[:, :-horizon], x[:, -horizon:]
--- so_ml_tools/timeseries/plot.py
+++ so_ml_tools/timeseries/plot.py
@@ -1,103 +1,48 @@
 import matplotlib.pyplot as _plt
 import seaborn as _sns
-import itertools as _itertools
 import numpy as _np
 import tensorflow as _tf
+import pandas as _pd
 
 
-def timeseries(x: list[_np.ndarray],
-               y: list[_np.ndarray],
-               labels: list[str] = None,
-               title: str = None,
-               x_label: str = None,
-               y_label: str = None,
-               figsize: tuple = (8, 6),
-               label_color='black') -> None:
-    """
-    Plots time series data, where `x` is a single array containing the date/time and `y` can
-    be one or more arrays of labels and predictions.
+def _to_numpy(x):
+    if not isinstance(x, _np.ndarray):
+        if isinstance(x, _pd.DataFrame) | isinstance(x, _pd.Series) | isinstance(x, _pd.DatetimeIndex):
+            return x.to_numpy()
+        elif isinstance(x, _tf.Tensor):
+            return x.numpy()
+        else:
+            return _tf.convert_to_tensor(value=x).numpy()
 
-    In case `y` has a larger horizon then one, for example to predict 7 days, then the data will be
-    reduced using the mean operation.
+    return x
 
-    The use this method you can call it in the following ways:
 
-    timeseries(
-        x=[
-            X_history,
-            X_pred
-        ],
-        y=[
-            y_history,
-            y_pred
-        ],
-        labels=['Label', 'Pred'],
-        start=300,
-        x_label='Time', y_label
-        ='Price')
-
-    Note that filling in the values for X and y might be different in certain circumstances, most often this has
-    to do with the shape, make sure they match.
-
-    Where day is set to the day in the range of the horizon, in this case the third day.
-
-    Args:
-        x: A list containing one ore more arrays of dates/times
-        y: A list containing one ore more arrays of labels and predictions
-        labels: An array of labels where each label corresponds to an entry in the list of y.
-        title: An optional title for the graph.
-        x_label: An optional x-axis label for the graph.
-        y_label: An optional y-axis label for the graph.
-        figsize: An optional figure size. Default is (8, 6)
-        label_color: An optional color for the labels of the axis, axis values and title.
-    """
+def plot_predictions(x, y_true, y_prob, start_index=None, end_index=None, title='Predictions', figsize=(30, 8)):
     fig, ax = _plt.subplots(figsize=figsize)
 
-    color_palette = _itertools.cycle(_sns.color_palette(palette="tab10", n_colors=len(x)))
-    for idx, _y in enumerate(y):
-        _x = x[idx]
+    x = _to_numpy(x)
+    y_true = _to_numpy(y_true)
+    y_prob = _to_numpy(y_prob)
 
-        # If the horizon of the data contains more then one day then reduce the
-        if hasattr(_y, 'ndim') and _y.ndim > 1:
-            _y = _tf.reduce_mean(_y, axis=1)
-            if labels is not None and len(labels) > idx:
-                labels[idx] = f"{labels[idx]} (mean)"
-        else:
-            _y = y[idx]
+    if start_index is None:
+        start_index = 0
 
-        _label = 'None'
-        if labels is not None and len(labels) > idx:
-            _label = labels[idx - 1]
+    if end_index is None:
+        end_index = len(x)
 
+    assert end_index > start_index, 'end_index must be larger then start_index'
 
+    print(f'{start_index}, {end_index}')
 
-        _sns.lineplot(
-            x=_x,
-            y=_y,
-            color=next(color_palette),
-            # s=7,
-            ax=ax,
-            label=_label)
+    _sns.lineplot(x=x.ravel()[start_index:end_index],
+                  y=y_true.ravel()[start_index:end_index],
+                  ax=ax)
 
-    if x_label:
-        _plt.xlabel(x_label)
-    if y_label:
-        _plt.ylabel(y_label)
-    if title:
-        _plt.title(title)
+    _sns.lineplot(x=x.ravel()[start_index:end_index],
+                  y=y_prob.ravel()[start_index:end_index],
+                  ax=ax)
 
-    fig.patch.set_alpha(0.0)  # Transparant background
-
-    ax.tick_params(axis='x', labelrotation=90)
-
-    ax.xaxis.label.set_color(label_color)  # Set color of x-axis label
-    ax.tick_params(axis='x', colors=label_color)  # Set color of x-axis ticks.
-
-    ax.yaxis.label.set_color(label_color)  # Set color of y-axis label
-    ax.tick_params(axis='y', colors=label_color)  # Set color of y-axis ticks.
-    ax.title.set_color(label_color)  # Set color of title
-
-    if labels:
-        _plt.legend()
-
+    _plt.title(f'{title} (range: {start_index}-{end_index})')
+    ax.set_ylabel("Value")
+    ax.set_xlabel("Time")
     _plt.show()

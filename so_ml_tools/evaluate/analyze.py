3b4b7b1cb5b8aa86d0c7678f454f337ad36beec9
Ronald Mathies <github@sodeso.nl>
2024-03-14 14:16:47 +0100

    Added timeseries window function and moved some packages.

--- so_ml_tools/evaluate/analyze.py
+++ so_ml_tools/evaluate/analyze.py
@@ -4,6 +4,33 @@ import sklearn as _sklearn
 import so_ml_tools as _soml
 from typing import Union as _Union
 
+import tensorflow as _tf
+import numpy as _np
+import pandas as _pd
+from so_ml_tools.tf.loss.mase import mean_absolute_scaled_error as _mean_absolute_scaled_error
+
+
+def evaluate_preds(y_true, y_pred, seasonality: int = None) -> dict:
+    y_true = _to_numpy(y_true)
+    y_pred = _to_numpy(y_pred)
+
+    y_true = y_true.astype(dtype=_np.float32).ravel()
+    y_pred = y_pred.astype(dtype=_np.float32).ravel()
+
+    mae = _tf.keras.metrics.mean_absolute_error(y_true=y_true, y_pred=y_pred).numpy()
+    mse = _tf.keras.metrics.mean_squared_error(y_true=y_true, y_pred=y_pred).numpy()
+    rmse = _np.sqrt(mse)
+    mape = _tf.keras.metrics.mean_absolute_percentage_error(y_true=y_true, y_pred=y_pred).numpy()
+    mase = _mean_absolute_scaled_error(y_true=y_true, y_pred=y_pred, seasonality=seasonality)
+
+    return {
+        'mae': mae,
+        'mse': mse,
+        'rmse': rmse,
+        'mape': mape,
+        'mase': mase
+    }
+
 
 def determine_outliers_for_multiclass_classification(x, y_true, y_pred=None, y_prob=None, top=10) -> _pd.DataFrame:
     """
@@ -193,3 +220,15 @@ def quality_metrics_combine(metrics: dict, sort_by: list[str] = None, ascending:
     all_metrics_results.rename(columns={"level_0": "name"}, inplace=True)
     all_metrics_results.sort_values(by=sort_by, inplace=True, ascending=ascending)
     return all_metrics_results
+
+
+def _to_numpy(x):
+    if not isinstance(x, _np.ndarray):
+        if isinstance(x, _pd.DataFrame) | isinstance(x, _pd.Series) | isinstance(x, _pd.DatetimeIndex):
+            return x.to_numpy()
+        elif isinstance(x, _tf.Tensor):
+            return x.numpy()
+        else:
+            return _tf.convert_to_tensor(value=x).numpy()
+
+    return x
--- so_ml_tools/tf/fit/__init__.py
+++ so_ml_tools/tf/fit/__init__.py
@@ -1,9 +1,7 @@
 from . import (
     callback,
-    metrics
 )
 
 __all__ = [
     "callback",
-    "metrics"
 ]

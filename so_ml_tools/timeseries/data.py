82e994016b5735ebf1513325135ff1332b7fe455
Ronald Mathies <github@sodeso.nl>
2024-04-08 11:57:44 +0200

    Added some convenience methods to timeseries.data

--- so_ml_tools/timeseries/data.py
+++ so_ml_tools/timeseries/data.py
@@ -2,6 +2,7 @@ import tensorflow as _tf
 import pandas as _pd
 import keras as _ks
 import numpy as _np
+import math as _math
 
 
 def make_naive_predictions(y_true):
@@ -100,12 +101,28 @@ def dataset_from_tensor_univariate(
         tensor: _tf.Tensor,
         window_size: int,
         horizon_size: int = 1,
+        centered: bool = False,
         batch_size=None) \
         -> _tf.data.Dataset:
+    """
+    Creates a TensorFlow Dataset from a given tensor object with the specified window size and horizon size. To
+    center the labels set the `centered=True` argument.
+
+    Args:
+        tensor: the tensor object
+        window_size: the size of the window
+        horizon_size: the size of the horizon (smaller or equal to window_size)
+        centered: flag to indicate if the labels should be centered in relation to the window
+        batch_size: batch size for the dataset
+
+    Returns:
+        the dataset.
+    """
     return dataset_from_array_univariate(
         array=tensor.numpy().ravel(),
         window_size=window_size,
         horizon_size=horizon_size,
+        centered=centered,
         batch_size=batch_size)
 
 
@@ -113,12 +130,28 @@ def dataset_from_dataframe_univariate(
         dataframe: _pd.DataFrame,
         window_size: int,
         horizon_size: int = 1,
+        centered: bool = False,
         batch_size=None) \
         -> _tf.data.Dataset:
+    """
+    Creates a TensorFlow Dataset from a given dataframe object with the specified window size and horizon size. To
+    center the labels set the `centered=True` argument.
+
+    Args:
+        dataframe: the dataframe object
+        window_size: the size of the window
+        horizon_size: the size of the horizon (smaller or equal to window_size)
+        centered: flag to indicate if the labels should be centered in relation to the window
+        batch_size: batch size for the dataset
+
+    Returns:
+        the dataset.
+    """
     return dataset_from_array_univariate(
         array=dataframe.to_numpy().ravel(),
         window_size=window_size,
         horizon_size=horizon_size,
+        centered=centered,
         batch_size=batch_size)
 
 
@@ -126,92 +159,45 @@ def dataset_from_array_univariate(
         array: _np.ndarray,
         window_size: int,
         horizon_size: int = 1,
+        centered: bool = False,
         batch_size=None) -> _tf.data.Dataset:
+    """
+    Creates a TensorFlow Dataset from a given array object with the specified window size and horizon size. To
+    center the labels set the `centered=True` argument.
+
+    Args:
+        array: the array object
+        window_size: the size of the window
+        horizon_size: the size of the horizon (smaller or equal to window_size)
+        centered: flag to indicate if the labels should be centered in relation to the window
+        batch_size: batch size for the dataset
+
+    Returns:
+        the dataset.
+    """
+    assert horizon_size <= window_size, f"horizon_size '{horizon_size}' must be smaller or equal to window_size '{window_size}'"
+    assert not centered or (
+                centered and window_size > 2), f"Centered window requested but window_size is too small: '{window_size}', needs to be at least 3 or higher."
+
     x = array.ravel()
-    y = x[window_size:]
 
-    return dataset_from_arrays_univariate(
-        x=x,
-        y=y,
-        window_size=window_size,
-        horizon_size=horizon_size,
-        batch_size=batch_size)
+    label_offset = window_size
+    if centered:
+        label_offset = _math.floor(window_size / 2)
 
+    y = x[label_offset:]
 
-def dataset_from_arrays_univariate(
-        x: _np.ndarray,
-        y: _np.ndarray,
-        window_size: int,
-        horizon_size: int = 1,
-        batch_size=None) -> _tf.data.Dataset:
-    x = x.ravel()
     if horizon_size > 1:
-        horizon_start_offset = window_size
-        horizon_end_offset = window_size + horizon_size
+        horizon_start_offset = label_offset
+        horizon_end_offset = label_offset + horizon_size
         y = [x[start_idx + horizon_start_offset:start_idx + horizon_end_offset]
              for start_idx, _ in enumerate(y)]
 
-        # Remove last set of lines which are not complete
-        y = y[0:-horizon_size + 1]
+        # Remove last set of labels which are not complete horizons
+        y = y[:-window_size + 1]
 
     return _ks.utils.timeseries_dataset_from_array(
         x,
         y,
         sequence_length=window_size,
         batch_size=batch_size)
-
-
-# def make_windows_univariate(x, window_size, horizon):
-#     """
-#     Turns 1 1D array into a 2D array of sequential labelled windows of window_size with horizon size labels.
-#
-#     E.g. if horizon = 1 and window_size = 3, then
-#     X = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-#
-#     Output:
-#         Features:
-#         [
-#             [0, 1, 2, 3, 4, 5, 6]
-#             [1, 2, 3, 4, 5, 6, 7]
-#         ]
-#
-#         Labels:
-#         [
-#             [7]
-#             [8]
-#         ]
-#
-#     Args:
-#         x: A 1D Numpy array or Tensor or a single column Pandas dataframe
-#         window_size: the number of time steps to use as features
-#         horizon:  the number of time steps to use as labels
-#
-#     """
-#     if not isinstance(x, _tf.Tensor):
-#         x = _tf.convert_to_tensor(value=x)
-#
-#     x = x.numpy().ravel()
-#
-#     # 1. Create a window of specific window_size (add the horizon on the end for labelling later)
-#     window_step = _np.expand_dims(_np.arange(window_size + horizon), axis=0)
-#
-#     # 2. Create a 2D array for multiple window step (minus 1 to account for 0 indexing)
-#     window_indexes = window_step + _np.expand_dims(_np.arange(len(x) - (window_size + horizon - 1)),
-#                                                   axis=0).T  # Create 2D array of windows of window size window_size.
-#
-#     # 3. Index on the target array (a time series) with 2D array of multiple window steps
-#     windowed_array = x[window_indexes]
-#
-#     # 4. get the labelled windows
-#     windows, labels = _get_labelled_windows(windowed_array, horizon=horizon)
-#     return windows, labels
-
-
-# def _get_labelled_windows(x, horizon):
-#     """
-#     Creates labels for windowed dataset.
-#
-#     E.g. if horizon = 1
-#     Input: [0, 1, 2, 3, 4, 5, 6, 7] -> Output: ([0, 1, 2, 3, 4, 5, 6], [7])
-#     """
-#     return x[:, :-horizon], x[:, -horizon:]

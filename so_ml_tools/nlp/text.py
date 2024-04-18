1c7e5dd32206f68e735f65f1ae37d919c4dad730
Ronald Mathies <github@sodeso.nl>
2024-04-18 15:13:04 +0200

    Fixed an issue with get_features_and_labels when the dataset is shuffled the result of the features and labels would not match.

--- so_ml_tools/nlp/text.py
+++ so_ml_tools/nlp/text.py
@@ -2,6 +2,8 @@ import numpy as _np
 import pandas as _pd
 import tensorflow as _tf
 import re as _re
+import so_ml_tools as _soml
+
 from typing import Union as _Union
 
 ########################################################################################################################
@@ -15,14 +17,14 @@ STRIP_PUNCTUATION = "strip_punctuation"
 LOWER = "lower"
 
 
-def count_unique_words(lines: _Union[list[str], _tf.Tensor], standardize="lower_and_strip_punctuation") -> int:
-    if _tf.is_tensor(x=lines):
-        lines = lines.numpy()
+def count_unique_words(corpus: _Union[list[str], _tf.Tensor, _np.ndarray, _pd.DataFrame, _pd.Series], standardize="lower_and_strip_punctuation") -> int:
+    if not isinstance(corpus, _np.ndarray):
+        corpus = _soml.util.types.to_numpy(corpus)
 
     regex = _re.compile(PUNCTUATION)
 
     unique = set()
-    for line in lines:
+    for line in corpus:
         if isinstance(line, (bytes, bytearray)):
             line = line.decode(encoding='utf-8')
 
@@ -34,17 +36,17 @@ def count_unique_words(lines: _Union[list[str], _tf.Tensor], standardize="lower_
     return len(unique)
 
 
-def word_count_for_sentences(lines: _Union[list[str], _tf.Tensor]):
-    if _tf.is_tensor(x=lines):
-        lines = lines.numpy()
+def word_count_for_sentences(corpus: _Union[list[str], _tf.Tensor, _np.ndarray, _pd.DataFrame, _pd.Series]):
+    if not isinstance(corpus, _np.ndarray):
+        corpus = _soml.util.types.to_numpy(corpus)
 
-    word_count_per_sentence = __count_words_for_each_sentence(lines)
+    word_count_per_sentence = __count_words_for_each_sentence(corpus)
     word_count_for_90_percentile = int(_np.percentile(a=word_count_per_sentence, q=90))
     word_count_for_95_percentile = int(_np.percentile(a=word_count_per_sentence, q=95))
     word_count_for_97_percentile = int(_np.percentile(a=word_count_per_sentence, q=97))
     word_count_for_99_percentile = int(_np.percentile(a=word_count_per_sentence, q=99))
     word_count_for_max = max(word_count_per_sentence)
-    word_count_for_average = round(sum(word_count_per_sentence) / len(lines))
+    word_count_for_average = round(sum(word_count_per_sentence) / len(corpus))
 
     print('Word count for x-percentile sentences:')
     print(f' 90%: {word_count_for_90_percentile}')
@@ -59,14 +61,15 @@ def word_count_for_sentences(lines: _Union[list[str], _tf.Tensor]):
 # Character level functions
 ########################################################################################################################
 
-def count_unique_chars(lines: _Union[list[str], _tf.Tensor], standardize="lower_and_strip_punctuation") -> (int, list[chr]):
+
+def count_unique_chars(corpus: _Union[list[str], _tf.Tensor, _np.ndarray, _pd.DataFrame, _pd.Series], standardize="lower_and_strip_punctuation") -> (int, list[chr]):
     regex = _re.compile(PUNCTUATION)
 
-    if _tf.is_tensor(x=lines):
-        lines = lines.numpy()
+    if not isinstance(corpus, _np.ndarray):
+        corpus = _soml.util.types.to_numpy(corpus)
 
     unique = set()
-    for line in lines:
+    for line in corpus:
         if isinstance(line, (bytes, bytearray)):
             line = line.decode(encoding='utf-8')
 
@@ -78,21 +81,21 @@ def count_unique_chars(lines: _Union[list[str], _tf.Tensor], standardize="lower_
     return len(unique), unique
 
 
-def length_per_sentence(lines: _Union[list[str], _tf.Tensor]) -> list[int]:
+def length_per_sentence(corpus: _Union[list[str], _tf.Tensor, _np.ndarray, _pd.DataFrame, _pd.Series]) -> list[int]:
     """
     Calculates the length for each string value and returns a list containing
     these lengths.
 
-    :param lines: a list of string values
+    :param corpus: a list of string values
     :return:
     """
-    if _tf.is_tensor(x=lines):
-        lines = lines.numpy()
+    if not isinstance(corpus, _np.ndarray):
+        corpus = _soml.util.types.to_numpy(corpus)
 
-    return [len(sentence) for sentence in lines]
+    return [len(sentence) for sentence in corpus]
 
 
-def calculate_q_precentile_character_lengths_for_all_sentence(lines: _Union[list[str], _tf.Tensor], q=95) -> int:
+def calculate_q_precentile_character_lengths_for_all_sentence(lines: _Union[list[str], _tf.Tensor, _np.ndarray, _pd.DataFrame, _pd.Series], q=95) -> int:
     """
     Calculates the q-percentile based on the lengths of the strings.
 
@@ -103,32 +106,28 @@ def calculate_q_precentile_character_lengths_for_all_sentence(lines: _Union[list
     return int(_np.percentile(a=length_per_sentence(lines), q=q))
 
 
-def calculate_average_character_length_for_all_sentences(lines: _Union[list[str], _tf.Tensor]) -> int:
+def calculate_average_character_length_for_all_sentences(corpus: _Union[list[str], _tf.Tensor, _np.ndarray, _pd.DataFrame, _pd.Series]) -> int:
     """
     Returns the average length of the strings.
 
-    :param lines: a list of string values
+    :param corpus: a list of string values
     :return: the average sentence length
     """
-    if _tf.is_tensor(x=lines):
-        lines = lines.numpy()
+    if not isinstance(corpus, _np.ndarray):
+        corpus = _soml.util.types.to_numpy(corpus)
 
-    return round(sum(length_per_sentence(lines)) / len(lines))
+    return round(sum(length_per_sentence(corpus)) / len(corpus))
 
 
-def __count_words_for_each_sentence(lines: _Union[list[str], _tf.Tensor, _pd.Series]) -> list[int]:
+def __count_words_for_each_sentence(corpus: _Union[list[str], _tf.Tensor, _np.ndarray, _pd.DataFrame, _pd.Series]) -> list[int]:
     """
     Calculates the length for each string value and returns a list containing
     these lengths.
 
-    :param lines: a list of string values
+    :param corpus: a list of string values
     :return:
     """
-    if isinstance(lines, _pd.DataFrame):
-        raise TypeError("lines is a Pandas DataFrame, select a series to calculate the length")
-    if isinstance(lines, _pd.Series):
-        lines = lines.numpy()
-    if _tf.is_tensor(x=lines):
-        lines = lines.numpy()
+    if not isinstance(corpus, _np.ndarray):
+        corpus = _soml.util.types.to_numpy(corpus)
 
-    return [len(i.split()) for i in lines]
+    return [len(i.split()) for i in corpus]
